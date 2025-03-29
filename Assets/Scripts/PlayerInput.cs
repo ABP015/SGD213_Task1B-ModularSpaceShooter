09399396a83461b6d17526013f7e5eb7536e1f6f
Alicia-Park <aliciaparkinson23@gmail.com>
2025-03-29 11:25:32 +1000

    commit 7

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour
+public class PlayerInput : MonoBehaviour
 {
 
     private Movement movement;
-    private TrippleShot shooting;
+    private IWeapon shooting;
 
     // Start is called before the first frame update
     void Start()
     {
         movement = GetComponent<Movement>();
-        shooting = GetComponent<TrippleShot>();
+        shooting = GetComponent<IWeapon>();
     }
 
     // Update is called once per frame
